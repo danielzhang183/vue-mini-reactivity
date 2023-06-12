c30a0e62809a9863b37c6181628cb42f54f58610
Dylan Zhang <dylanzhang@augmentum.com.cn>
2023-06-12 15:27:42 +0800

    refactor: extract track & trigger

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,28 +3,34 @@ import { activeEffect, buckets } from './effect'
 export function createProxy<T extends Object>(data: T) {
   return new Proxy(data, {
     get(target, key) {
-      if (!activeEffect)
-        return target[key]
-      let depsMap = buckets.get(target)
-      if (!depsMap)
-        buckets.set(target, (depsMap = new Map()))
-      let deps = depsMap.get(key)
-      if (!deps)
-        depsMap.set(key, (deps = new Set()))
-      deps.add(activeEffect)
-
+      track(target, key)
       return target[key]
     },
     set(target, key, newVal) {
       target[key] = newVal
-
-      const depsMap = buckets.get(target)
-      if (!depsMap)
-        return false
-      const effects = depsMap.get(key)
-      effects?.forEach(fn => fn())
-
-      return true
+      return trigger(target, key)
     },
   })
+}
+
+export function track(target: Object, key: string | symbol) {
+  if (!activeEffect)
+    return target[key]
+  let depsMap = buckets.get(target)
+  if (!depsMap)
+    buckets.set(target, (depsMap = new Map()))
+  let deps = depsMap.get(key)
+  if (!deps)
+    depsMap.set(key, (deps = new Set()))
+  deps.add(activeEffect)
+}
+
+export function trigger(target: Object, key: string | symbol): boolean {
+  const depsMap = buckets.get(target)
+  if (!depsMap)
+    return false
+  const effects = depsMap.get(key)
+  effects?.forEach(fn => fn())
+
+  return true
 }
